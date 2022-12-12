17cbd8e11063d0968d81ff0aff224f33c2bd55a7
saintsavon <jaylensavon@gmail.com>
2022-12-11 19:55:21 -0800

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Jaylen Scott on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
